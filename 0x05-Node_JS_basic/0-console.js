c81b78338e8bf6e54836538eaf97ba85725860ca
Leedasamurai <lesegoleemac@gmail.com>
2024-07-26 13:03:36 +0000

    remake

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,17 +1,6 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
+//0-console.js
+function displayMessage(message) {
+  console.log(message);
+}
 
-const displayMessage = (message) => {
-	console.log(message);
-};
-
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-// Default module export.
 module.exports = displayMessage;
